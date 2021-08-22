8ac2ade5adc8515038da0be7c44013a259e01c11
Anamitra Saikia <rusty.rx5@gmail.com>
2021-08-22 16:16:30 +0530

    .gitignore fix

--- migrations/3_factory.js
+++
@@ -1,9 +0,0 @@
-const base = artifacts.require("testbase");
-const factory = artifacts.require("CloneFactory");
-
-
-module.exports = function (deployer) {
-  deployer.deploy(base).then(()=>{
-    return deployer.deploy(factory, base.address );
-  })
-};
